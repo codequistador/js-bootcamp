94d5a0816f4d41cc64ac234d17fe8db5b7dbc8af
Justin Daining <jdaining@instructure.com>
2021-01-01 12:29:23 -0700

    remove http from mead request

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,7 +1,5 @@
 const getGame = async (wordCount) => {
-  const response = await fetch(
-    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
-  )
+  const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   if (response.status === 200) {
     const data = await response.json()
     return data.puzzle
